c28d3267b20c028dd2c4a1cca2b488c0d935089a
vherever <vherever@gmail.com>
2016-06-23 13:18:33 +0300

    email validation

---
+++ react-email-validate/src/components/LeadCapture.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var LeadCapture = React.createClass({
+    render: function() {
+        return (
+            <div className="col-sm-3">
+                <div className="panel panel-default">
+                    <div className="panel-body">
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = LeadCapture;
---
+++ react-email-validate/src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var EmailField = require('./components/EmailField.jsx');
+
+ReactDOM.render(<EmailField />, document.getElementById('content'));
